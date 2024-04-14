60844b5c6567b3eaff8edf47f4a2417528b7f5c7
Olga Kharkova <olgakharkova2009@rambler.ru>
2024-04-14 22:53:09 +0300

    add codeе название коммита

--- Program.cs
+++ Program.cs
@@ -2,3 +2,56 @@
 //длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
 //либо задать на старте выполнения алгоритма. 
 //При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+using System.Text;
+
+string[] StringArray(string[] array)
+    {
+        int count = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] newArray = new string[count];
+        count = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                newArray[count] = array[i];
+                count++;
+            }
+        }
+        return newArray;
+    }
+
+    void PrintArray(string[] array)
+    {
+        foreach (var item in array)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+//string[] array = {"apple", "banana", "car", "dog", "elephant", "fox" }; // если массив задан, то следующие 9 строчек не нужны.
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+Console.Write("Введите количество строк: ");
+int n = Convert.ToInt32(Console.ReadLine()); 
+string[] array = new string[n]; 
+    for (int i = 0; i < n; i++)
+    {
+    array[i] = Console.ReadLine()!; 
+    }
+          
+string[] newArray = StringArray(array);
+
+Console.WriteLine("\n Новый массив:");
+ PrintArray(newArray);
